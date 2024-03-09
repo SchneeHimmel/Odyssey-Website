7d6e401195c5db74f6557fd82a949d45a0da6b47
Hikki <henriquesfj@gmail.com>
2024-03-09 19:13:40 -0300

    created gallery and its dependencies; slick sliders moved to gallery and modified

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-$('.image-slides').slick({
-  dots: true,
-  infinite: true,
-  Speed: 300,
+// $('.image-slides').slick({
+//   dots: true,
+//   infinite: true,
+//   Speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+//   autoplay: true,
+//   autoplaySpeed: 2000
+// });
+
+$('.photo-gallery').slick({
   slidesToShow: 1,
-  adaptiveHeight: true,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
   autoplay: true,
-  autoplaySpeed: 2000
+  autoplaySpeed: 2250,
+  asNavFor: '.photo-nav'
+});
+$('.photo-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.photo-gallery',
+  centerMode: true,
+  focusOnSelect: true
 });
